665c43f740bf7ddcbab000ff48f67edd3db8ea19
marvgm <marvgm@gmail.com>
2020-08-04 19:44:56 -0300

    renomeado projeto basico e criado outro com o mesmo nome seguindo o template

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # vendas-angular9-spring-boot
+
+Comandos angular:
+
+-Criar projeto angular com arquivo de rotas e css
+ng new clientes-app --routing
+
+--abrir vs code na pasta
+code .
+
+usar o awsome font
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
+
+instalar o jquery(depois ver no package.json:  "jquery": "^3.5.1")
+npm install --save jquery
+
+
+instalar o boststrap(depois fazer referencia no angular.json em build, sysles e scripts)
+npm install --save bootstrap@4.3.1
+
+--compilar e rodar a app angular(open abre o brownser)
+ng serve --open
+
--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -1,24 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ClientesModule } from './clientes/clientes.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
-import { CursoAngularComponent } from './curso-angular/curso-angular.component';
-import { TesteComponent } from './teste/teste.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HelloComponent,
-    CursoAngularComponent,
-    TesteComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ClientesModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
