4ffa744deb69a74e6817412309633b7ef3f8a424
marvgm <marvgm@gmail.com>
2020-09-18 18:57:53 -0300

    inicio api de servico prestado

---
+++ clientes/src/main/java/io/github/marvgm/clientes/model/repository/ServicoPrestadoRepository.java
@@ -0,0 +1,7 @@
+package io.github.marvgm.clientes.model.repository;
+
+import io.github.marvgm.clientes.model.entity.ServicoPrestado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicoPrestadoRepository extends JpaRepository<ServicoPrestado, Integer> {
+}
--- clientes/src/main/java/io/github/marvgm/clientes/model/repository/ServicoRepository.java
+++
@@ -1,7 +0,0 @@
-package io.github.marvgm.clientes.model.repository;
-
-import io.github.marvgm.clientes.model.entity.Servico;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServicoRepository extends JpaRepository<Servico, Integer> {
-}
